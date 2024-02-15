7803214c34e958f25d01be72efad03b4a0938724
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-02-15 11:37:20 +0000

    duplicated activity 3 files to activity 4

---
+++ Week01/Activity 4/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+const port = 2319;
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Login Page</h1>');
+});
+
+app.get('/tickets', (req, res) => {
+    res.send('<h1>Tickets Page</h1>');
+});
+
+app.get('/users', (req, res) => {
+    res.send('<h1>Users Page</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
