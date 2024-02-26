8e501a3e8cc16894704c36cd22264a7bbc156bf7
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-02-26 12:01:14 +0000

    Reverted activity 3 to hello world functionality

--- Week01/Activity 3/index.js
+++ Week01/Activity 3/index.js
@@ -1,20 +1,7 @@
-import express from 'express';
-
-const app = express();
-const port = 2319;
-
-app.get('/login', (req, res) => {
-    res.send('<h1>Login Page</h1>');
-});
-
-app.get('/tickets', (req, res) => {
-    res.send('<h1>Tickets Page</h1>');
-});
-
-app.get('/users', (req, res) => {
-    res.send('<h1>Users Page</h1>');
-});
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+"use strict";
+import * as http from "http";
+const port = 8080;
+http.createServer((req, res) => 
+{ 
+    res.end("Hello World"); 
+}).listen(port);
