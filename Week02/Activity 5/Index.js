bd505e53e62b82b3274dc63ae074ac62b1792e16
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-03-07 12:35:28 +0000

    Added auth user callback to tickets, user routes

--- Week02/Activity 5/Index.js
+++ Week02/Activity 5/Index.js
@@ -3,6 +3,15 @@ import express from 'express';
 const app = express();
 const port = 2319;
 
+app.use((req, res, next) => {
+    console.log(`Request made from IP: ${req.ip} for resource: ${req.originalUrl}`);
+    next()
+});
+
+function auth_user(req, res, next) {
+    res.redirect("/login");
+}
+
 app.get('/', (req, res) => {
     res.send(`
         <h1 style="color: red;">Hello World</h1>
@@ -15,13 +24,13 @@ app.get('/login', (req, res) => {
     res.send('<h1>Login Page</h1>');
 });
 
-// app.get('/tickets', (req, res) => {
-//     res.send('<h1>Tickets Page</h1>');
-// });
+app.get("/tickets", auth_user, (req, res) => {
+    res.send("<h1>Tickets</h1>");
+});
 
-// app.get('/users', (req, res) => {
-//     res.send('<h1>Users Page</h1>');
-// });
+app.get("/users", auth_user, (req, res) => {
+    res.send("<h1>Users</h1>");
+});
 
 app.get('*', (req, res) => {
     console.log('Redirecting to login...'.yellow);
