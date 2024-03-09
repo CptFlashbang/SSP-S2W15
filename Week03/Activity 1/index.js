7b4ac5d3ec12ae1d22109ad949faa65129bcb9a2
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-03-09 01:37:26 +0000

    Added application constraints

--- Week03/Activity 1/index.js
+++ Week03/Activity 1/index.js
@@ -5,4 +5,13 @@ import express from "express";
 
 // Application Constants 
 const app = express();
+const port = 2319;
 
+// Configuration 
+app.disable("x-powered-by"); 
+app.set("view engine", "ejs");
+
+// Application Listen
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
