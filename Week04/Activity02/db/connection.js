98547116980fb4ae74c2be68ff00d90aa54b8158
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-03 18:18:46 +0100

    establish constants

--- Week04/Activity02/db/connection.js
+++ Week04/Activity02/db/connection.js
@@ -1,3 +1,9 @@
 'use strict'
 // Imports
 import { MongoClient } from "mongodb";
+
+// Module Constants
+const dbName = "SSP";
+const dbUName = encodeURIComponent("<your_username>");
+const dbPass = encodeURIComponent("<your_password>");
+const url = `mongodb+srv://${dbUName}:${dbPass}@<your_link>`;
