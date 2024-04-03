2eea40db95f2bf68b925a82d9dfe2019659a0899
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-03 19:33:52 +0100

    added router

--- Week04/Activity03/index.js
+++ Week04/Activity03/index.js
@@ -2,10 +2,13 @@ import * as path from "path";
 import {fileURLToPath} from "url";
 import tokenRouter from "./routes/tokens.js";
 
-// Use File Location of Index and Get Dir
+// Use File Location of Index and Get Dir -------------------------------------------------------
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Preroute MIDDLEWARE -------------------------------------------------------
 app.use(express.static(path.join(__dirname,"public")));
 app.use("/favicon.ico", express.static("public/assets/ico/SSP.ico"));
+
+// ROUTER -------------------------------------------------------
+app.use("/tokens", tokenRouter);
