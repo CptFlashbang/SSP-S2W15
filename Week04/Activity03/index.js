17554e731b79ca04e9ca0853575a9f0e2c8bbaf6
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-03 17:42:38 +0100

    added objets for path handling

--- Week04/Activity03/index.js
+++ Week04/Activity03/index.js
@@ -4,3 +4,7 @@ import express from "express";
 import * as path from "path";
 import { fileURLToPath } from "url";
 import userRouter from "./routes/users.js";
+
+// Application Objects
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(__filename);
