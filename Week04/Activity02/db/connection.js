29d9a44edea1d415fa9992c0b609793e0e1fe3a1
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-03 18:28:20 +0100

    created module object

--- Week04/Activity02/db/connection.js
+++ Week04/Activity02/db/connection.js
@@ -7,3 +7,9 @@ const dbName = "SSP";
 const dbUName = encodeURIComponent("CallumYates");
 const dbPass = encodeURIComponent("C@bbage9829");
 const url = `mongodb+srv://${dbUName}:${dbPass}@ssp.slyivjn.mongodb.net/?retryWrites=true&w=majority&appName=SSP`;
+
+// Module Objects
+const dbClient = new MongoClient(url);
+// Module Variables
+let conn;
+let db;
