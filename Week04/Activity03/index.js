0403b484fd2afee4a6dbc5aa5b1bb6585727a625
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-03 17:46:17 +0100

    added app config

--- Week04/Activity03/index.js
+++ Week04/Activity03/index.js
@@ -8,3 +8,11 @@ import userRouter from "./routes/users.js";
 // Application Objects
 const __filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(__filename);
+
+// Application Objects
+const app = express();
+
+// Configuration
+app.disable("x-powered-by");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
