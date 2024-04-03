9dfb4356ea7f03d369ff06ae5e6c31344edaa28c
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-03 19:09:27 +0100

    created router instance

--- Week04/Activity03/routes/tokens.js
+++ Week04/Activity03/routes/tokens.js
@@ -4,3 +4,9 @@ import bodyParser from "body-parser";
 import express from "express";
 import db from "../db/connection.js";
 import { ObjectId } from "mongodb";
+
+// Application Objects
+const router = express.Router();
+// Middleware for This Router
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
