8749edca859aab444b7a000b8a80954aa940ca1a
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-03 17:41:51 +0100

    added imports to index file

--- Week04/Activity03/index.js
+++ Week04/Activity03/index.js
@@ -1,10 +1,6 @@
+'use strict'
+// Imports
+import express from "express";
 import * as path from "path";
-import {fileURLToPath} from "url";
-
-// Use File Location of Index and Get Dir
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Preroute MIDDLEWARE -------------------------------------------------------
-app.use(express.static(path.join(__dirname,"public")));
-app.use("/favicon.ico", express.static("public/assets/ico/SSP.ico"));
+import { fileURLToPath } from "url";
+import userRouter from "./routes/users.js";
