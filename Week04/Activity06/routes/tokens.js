4aef223e6b2c5503d94fa35b1f236d879ad1d21d
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-04-04 12:57:07 +0100

    Added route for buy-tokens to parse database information to view

--- Week04/Activity06/routes/tokens.js
+++ Week04/Activity06/routes/tokens.js
@@ -9,12 +9,18 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 // Middleware for This Router
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes for This Router (all routes /tokens onwards)
-router.get('/', (req, res) =>
-{
-res.render("tokens");
+router.get('/', (req, res) => {
+    res.render("tokens");
+});
+
+router.get('/buy-tokens', async (req, res) => {
+    // Local Variables 
+    let collection = await db.collection("Tokens");
+    let results = await collection.find({}).toArray();
+    res.render("buy-tokens", { games: results });
 });
 
 export default router;
