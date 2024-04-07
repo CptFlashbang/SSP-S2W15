764415dc361e18bde489591237d70b504c300e6f
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-04-07 14:21:38 +0100

    added firebase config data

--- Week05/Activity04/routes/users.js
+++ Week05/Activity04/routes/users.js
@@ -6,14 +6,23 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import multer from "multer";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"; 
-import admin from "firebase-admin"; 
-import * as path from "path"; import { fileURLToPath } from "url"; 
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import admin from "firebase-admin";
+import * as path from "path"; import { fileURLToPath } from "url";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 
 // Router Constants 
-const __filename = fileURLToPath(import.meta.url); 
-const __dirname = path.dirname(__filename); 
-const data = require(path.join(__dirname,"../key.json"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const data = require(path.join(__dirname, "../key.json"));
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB_q2Czzwc63A-gR9gD3h_i6taVxtHOq5w",
+    authDomain: "ssp-nodejs-13ac2.firebaseapp.com",
+    projectId: "ssp-nodejs-13ac2",
+    storageBucket: "ssp-nodejs-13ac2.appspot.com",
+    messagingSenderId: "313657043897",
+    appId: "1:313657043897:web:4ad29cf21374065b652468"
+  };
