2c3a271561a88948be5350d35c18f4ab4f9b7f52
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-04-07 13:50:21 +0100

    added constants for route

--- Week05/Activity04/routes/users.js
+++ Week05/Activity04/routes/users.js
@@ -12,3 +12,8 @@ import * as path from "path"; import { fileURLToPath } from "url";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
+
+// Router Constants 
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
+const data = require(path.join(__dirname,"../key.json"));
