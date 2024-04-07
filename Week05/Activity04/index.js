a1a5a9c48696adbc6e5f319f249c3948ead9c8b1
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-04-07 16:21:22 +0100

    corrected variable name

--- Week05/Activity04/index.js
+++ Week05/Activity04/index.js
@@ -7,7 +7,7 @@ import userRouter from "./routes/users.js";
 
 // Application Objects
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 // Application Objects
 const app = express();
