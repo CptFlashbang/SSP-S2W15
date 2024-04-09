30bd80708feb67375c14b9bf01c3522614d95446
CptFlashbang <Callum.yates.180@gmail.com>
2024-04-09 14:07:52 +0100

    Add user router and configure static file serving

---
+++ Assignment02/index.js
@@ -0,0 +1,37 @@
+'use strict'
+// Imports
+import express from "express";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import userRouter from "./routes/users.js";
+
+// Application Objects
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Application Objects
+const app = express();
+
+// Configuration
+app.disable("x-powered-by");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Preroute MIDDLEWARE -------------------------------------------------------
+app.use(express.static(path.join(__dirname,"public")));
+// app.use("/favicon.ico", express.static("public/assets/ico/favicon.ico"));
+
+// ROUTERS -------------------------------------------------------------------
+app.use("/users", userRouter);
+
+// ROUTES ---------------------------------------------------------------------
+app.get("/", (req, res) =>
+{
+res.render("home_unlogged");
+});
+
+// START SERVER --------------------------------------------------------------
+app.listen(2319, () =>
+{
+console.log("The server is listening...");
+});
